f88084e3a26a9a10d64a16dfea74fad32d01a0ac
Vitaliy Podolskiy <instarobot.net@gmail.com>
2020-04-05 11:24:46 +0300

    init project

---
+++ focusmusic.fm/focusmusic.fm/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  focusmusic.fm
+//
+//  Created by Vitaliy Podolskiy on 05.04.2020.
+//  Copyright © 2020 DEVLAB Studio, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
