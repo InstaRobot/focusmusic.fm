538e8a230f69fba8fe1370dddb368f91c83799e8
Vitaliy Podolskiy <instarobot.net@gmail.com>
2020-04-05 11:52:36 +0300

    finish first version

--- focusmusic.fm/focusmusic.fm/Popover/PopoverViewController.swift
+++ focusmusic.fm/focusmusic.fm/Popover/PopoverViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import Cocoa
+import WebKit
+
+extension WKWebView {
+    func load(_ urlString: String) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+}
 
 class PopoverViewController: NSViewController {
 
+    @IBOutlet private(set) weak var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        if let url = URL(string: "https://focusmusic.fm") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+
+    @IBAction private func reload(_ sender: Any) {
+        webView.load("https://focusmusic.fm")
     }
     
 }
